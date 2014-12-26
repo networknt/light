f97d0dd719c013a3d5fc5c7709cc62f511a28d1e
Steve Hu <stevehu@gmail.com>
2014-12-25 23:01:29 -0500

    more comment and signinCtrl and remove logging

--- networknt/app/scripts/controllers/signin.js
+++ networknt/app/scripts/controllers/signin.js
@@ -40,28 +40,27 @@ angular.module('lightApp').controller('signinCtrl', ['$rootScope', '$scope', '$h
             $scope.action[0].data = $scope.modelData;
             $http.post('/api/rs', $scope.action[0])
                 .success(function (data, status, headers, config) {
+                    // Successfully logged in and get access token back. If remember me is checked, then a refresh token is returned as well.
                     authService.authentication.isAuth = true;
+                    // Parse the Json Token and get uesr object which contains userId and roles.
                     authService.authentication.currentUser = JSON.parse(base64.base64Decode(data.accessToken.split('.')[1])).user;
+                    // This is an indicator if refresh token will be used to get another access token after access token is expired.
                     authService.authentication.useRefreshTokens = $scope.modelData.rememberMe;
+                    // Save authorizationData object into local storage so it can last longer than the browser session. local storage will
+                    // fall back to Cookie if HTML5 is not supported by the browser.
                     localStorageService.set('authorizationData', { token: data.accessToken, currentUser: authService.authentication.currentUser, refreshToken: data.refreshToken || '', useRefreshTokens: authService.authentication.useRefreshTokens  });
+                    // Redirect to the attempted url if the login page was redirected upon 401 and 403 error.
                     httpBuffer.redirectToAttemptedUrl();
-                    console.log('signinCtrl: authorizationData', localStorageService.get('authorizationData'));
-                    //console.log(data);
-                    //console.log(status);
-                    //console.log(headers);
-                    //console.log(config);
                 })
                 .error(function (data, status, headers, config) {
+                    // Clean the authorizatonData from local storage.
                     authService.logOut();
+                    // Clean all the saved requests in httpBuffer if this is redirected from failure of refreshing token.
                     httpBuffer.rejectAll();
                     if(angular.isDefined($scope.action[0].error)) {
                         $location.path($scope.action[0].error);
                     }
                     toaster.pop('error', status, data, 5000);
-                    //console.log(data);
-                    //console.log(status);
-                    //console.log(headers);
-                    //console.log(config);
                 });
         }
     };
