7630672120f35b5ea903d40885c6708bc1583586
Steve Hu <stevehu@gmail.com>
2015-01-14 15:48:33 -0500

    pattern

--- server/src/main/java/com/networknt/light/server/Json.java
+++ server/src/main/java/com/networknt/light/server/Json.java
@@ -3,21 +3,41 @@ package com.networknt.light.server;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Json {
 	public static void main(String[] args) { 
-		ObjectMapper mapper = new ObjectMapper();
-	    Map<String, String> parsedMap = null;
+		String injected = "{\"value\": \"value1\", \"label\": \"label1\"},\n" +
+				"          {\"value\": \"value2\", \"label\": \"label2\"},\n" +
+				"          {\"value\": \"value3\", \"label\": \"label3\"}\n";
 		try {
-            InputStream is = Json.class.getResourceAsStream("/form/com.networknt.light.class.feed.json");
-            System.out.println("is = " + is);
-            parsedMap = mapper.readValue(is,new TypeReference<HashMap<String, Object>>(){});
-            System.out.println("parsedMap = " + parsedMap);
+            InputStream is = Json.class.getResourceAsStream("/form/com.networknt.light.demo.uiselect_d.json");
+            String formStr = convertStreamToString(is);
+			//System.out.println("is = " + formStr);
+
+			Pattern pattern = Pattern.compile("\\[\"@\",([^]]+)\\]");
+
+			Matcher m = pattern.matcher(formStr);
+			StringBuffer sb = new StringBuffer(formStr.length());
+			while (m.find()) {
+				String text = m.group(1);
+				System.out.println("text = " + text);
+				// ... possibly process 'text' ...
+				m.appendReplacement(sb, Matcher.quoteReplacement(injected));
+			}
+			m.appendTail(sb);
+			System.out.println("form = " + sb.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	static String convertStreamToString(java.io.InputStream is) {
+		java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
 	}
 }
