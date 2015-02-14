5849f03f7efe75e2c6f55b21f39d18d7e7cbcba6
Steve Hu <stevehu@gmail.com>
2015-02-14 15:58:35 -0500

    check if page content has been changed or not before loading it. It doesn't work for some files.

---
+++ rule/src/main/java/com/networknt/light/rule/page/GetPageMapRule.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Network New Technologies Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.networknt.light.rule.page;
+
+import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
+import com.networknt.light.rule.Rule;
+import com.networknt.light.util.ServiceLocator;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.serialization.serializer.OJSONWriter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by steve on 14/02/15.
+ *
+ * This is the rule that called by page:load plugin in maven-plugin repo.
+ * It get all the page and return a map from id to content.
+ *
+ * accessLevel is owner by default.
+ *
+ */
+public class GetPageMapRule extends AbstractPageRule implements Rule {
+    public boolean execute (Object ...objects) throws Exception {
+        Map<String, Object> inputMap = (Map<String, Object>) objects[0];
+        Map<String, Object> data = (Map<String, Object>) inputMap.get("data");
+        Map<String, Object> payload = (Map<String, Object>) inputMap.get("payload");
+        Map<String, Object> user = (Map<String, Object>)payload.get("user");
+        String host = (String)user.get("host");
+
+        String pages = getPageMap(host);
+        if(pages != null) {
+            inputMap.put("result", pages);
+            return true;
+        } else {
+            inputMap.put("result", "No page can be found.");
+            inputMap.put("responseCode", 404);
+            return false;
+        }
+    }
+}
--- server/src/main/java/com/networknt/light/server/handler/loader/PageLoader.java
+++ server/src/main/java/com/networknt/light/server/handler/loader/PageLoader.java
@@ -16,6 +16,7 @@
 
 package com.networknt.light.server.handler.loader;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.networknt.light.util.ServiceLocator;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -36,7 +37,8 @@ import java.util.Scanner;
  * Created by husteve on 10/24/2014.
  */
 public class PageLoader extends Loader {
-    static public String pageFolder = "page";
+    static String pageFolder = "page";
+    static Map<String, String> pageMap = null;
 
     public static void main(String[] args) {
         try {
@@ -61,6 +63,8 @@ public class PageLoader extends Loader {
                 httpclient = HttpClients.createDefault();
                 // login as owner here
                 login(host, userId, password);
+                getPageMap(host);
+
                 File[] listOfFiles = folder.listFiles();
                 for (int i = 0; i < listOfFiles.length; i++) {
                     loadPageFile(host, listOfFiles[i]);
@@ -73,42 +77,93 @@ public class PageLoader extends Loader {
         }
     }
 
+    /**
+     * Get all pages from the server and construct a map in order to compare content
+     * to detect changes or not.
+     *
+     */
+    private static void getPageMap(String host) {
+
+        Map<String, Object> inputMap = new HashMap<String, Object>();
+        inputMap.put("category", "page");
+        inputMap.put("name", "getPageMap");
+        inputMap.put("readOnly", true);
+
+        CloseableHttpResponse response = null;
+        try {
+            HttpPost httpPost = new HttpPost(host + "/api/rs");
+            httpPost.addHeader("Authorization", "Bearer " + jwt);
+            StringEntity input = new StringEntity(ServiceLocator.getInstance().getMapper().writeValueAsString(inputMap));
+            input.setContentType("application/json");
+            httpPost.setEntity(input);
+            response = httpclient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()));
+            String json = "";
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                json = json + line;
+            }
+            EntityUtils.consume(entity);
+            System.out.println("Got page map from server");
+            pageMap = ServiceLocator.getInstance().getMapper().readValue(json,
+                    new TypeReference<HashMap<String, String>>() {
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     private static void loadPageFile(String host, File file) {
         Scanner scan = null;
         try {
             scan = new Scanner(file, Loader.encoding);
             // the content is only the data portion. convert to map
             String content = scan.useDelimiter("\\Z").next();
-            Map<String, Object> inputMap = new HashMap<String, Object>();
-            inputMap.put("category", "page");
-            inputMap.put("name", "impPage");
-            inputMap.put("readOnly", false);
-            Map<String, Object> data = new HashMap<String, Object>();
             String id = file.getName();
             id = id.substring(0, id.lastIndexOf('.'));
-            data.put("id", id);
-            data.put("content", content);
-            inputMap.put("data", data);
-            HttpPost httpPost = new HttpPost(host + "/api/rs");
-            httpPost.addHeader("Authorization", "Bearer " + jwt);
-            StringEntity input = new StringEntity(ServiceLocator.getInstance().getMapper().writeValueAsString(inputMap));
-            input.setContentType("application/json");
-            httpPost.setEntity(input);
-            CloseableHttpResponse response = httpclient.execute(httpPost);
+            if(content != null && !content.equals(pageMap.get(id))) {
+                System.out.println(content);
+                System.out.println(pageMap.get(id));
+                Map<String, Object> inputMap = new HashMap<String, Object>();
+                inputMap.put("category", "page");
+                inputMap.put("name", "impPage");
+                inputMap.put("readOnly", false);
+                Map<String, Object> data = new HashMap<String, Object>();
+                data.put("id", id);
+                data.put("content", content);
+                inputMap.put("data", data);
+                HttpPost httpPost = new HttpPost(host + "/api/rs");
+                httpPost.addHeader("Authorization", "Bearer " + jwt);
+                StringEntity input = new StringEntity(ServiceLocator.getInstance().getMapper().writeValueAsString(inputMap));
+                input.setContentType("application/json");
+                httpPost.setEntity(input);
+                CloseableHttpResponse response = httpclient.execute(httpPost);
 
-            try {
-                System.out.println("Page: " + file.getAbsolutePath() + " is loaded with status " + response.getStatusLine());
-                HttpEntity entity = response.getEntity();
-                BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()));
-                String json = "";
-                String line = "";
-                while ((line = rd.readLine()) != null) {
-                    json = json + line;
+                try {
+                    System.out.println("Page: " + file.getAbsolutePath() + " is loaded with status " + response.getStatusLine());
+                    HttpEntity entity = response.getEntity();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()));
+                    String json = "";
+                    String line = "";
+                    while ((line = rd.readLine()) != null) {
+                        json = json + line;
+                    }
+                    //System.out.println("json = " + json);
+                    EntityUtils.consume(entity);
+                } finally {
+                    response.close();
                 }
-                //System.out.println("json = " + json);
-                EntityUtils.consume(entity);
-            } finally {
-                response.close();
+            } else {
+                System.out.println("Skip file " + id);
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
