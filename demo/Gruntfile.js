8b31beacc075cd7b1755f4e1aaf82fff1425b6d6
Steve Hu <stevehu@gmail.com>
2015-03-31 21:13:21 -0400

    createa new user for demo

--- demo/Gruntfile.js
+++ demo/Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '/home/networknt/web'
+    dist: '/home/demo/web'
   };
 
   // Define the configuration for all the tasks
