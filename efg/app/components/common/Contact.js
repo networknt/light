b17b3c25b8d7840f73268004a5840c03d04e62d0
Steve Hu <stevehu@gmail.com>
2015-07-11 07:29:11 -0400

    make menu works

---
+++ efg/app/components/common/Contact.js
@@ -0,0 +1,14 @@
+/**
+ * Created by steve on 11/07/15.
+ */
+var React = require('react');
+
+var Contact = React.createClass({
+    render: function() {
+        return (
+            <div>Contact</div>
+        );
+    }
+});
+
+module.exports = Contact;
--- efg/app/routes.js
+++ efg/app/routes.js
@@ -12,11 +12,45 @@ var Login = require('./components/auth/Login.js');
 //var Story = require('./components/stories/StoryPage.react.jsx');
 //var StoryNew = require('./components/stories/StoryNew.react.jsx');
 var Signup = require('./components/auth/Signup.js');
+var About = require('./components/common/About.js');
+var Contact = require('./components/common/Contact.js');
+
+var Admin = React.createClass({
+    render: function() {
+        return <div>Admin</div>;
+    }
+});
+
+var RoleAdmin = React.createClass({
+    render: function() {
+        return <div>RoleAdmin</div>;
+    }
+});
+var UserAdmin = React.createClass({
+    render: function() {
+        return <div>UserAdmin</div>;
+    }
+});
+var BlogAdmin = React.createClass({
+    render: function() {
+        return <div>BlogAdmin</div>;
+    }
+});
+var AccessAdmin = React.createClass({
+    render: function() {
+        return <div>AccessAdmin</div>;
+    }
+});
 
 module.exports = (
     <Route name="app" path="/" handler={App}>
+        <Route name="about" path="/about" handler={About} />
+        <Route name="contact" path="/contact" handler={Contact} />
+        <Route name="roleAdmin" path="/admin/roleAdmin" handler={RoleAdmin} />
+        <Route name="userAdmin" path="/admin/userAdmin" handler={UserAdmin} />
+        <Route name="blogAdmin" path="/admin/blogAdmin" handler={BlogAdmin} />
+        <Route name="accessAdmin" path="/admin/accessAdmin" handler={AccessAdmin} />
         <Route name="login" path="/login" handler={Login}/>
         <Route name="signup" path="/signup" handler={Signup}/>
     </Route>
 );
-
