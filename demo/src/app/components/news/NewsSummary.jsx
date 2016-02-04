e47c4fe0e141e932f5c9a36811a5ddfe41742aae
Steve Hu <stevehu@gmail.com>
2016-02-03 22:47:50 -0500

    add moment and format date in blog and news summary

--- demo/src/app/components/news/NewsSummary.jsx
+++ demo/src/app/components/news/NewsSummary.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Paper from 'material-ui/lib/paper';
 import Gravatar from '../Gravatar';
 import Markdown from '../Markdown';
+import moment from 'moment';
 
 class NewsSummary extends React.Component {
 
     render() {
+        let time = moment(this.props.post.createDate).format("DD-MM-YYYY HH:mm:ss");
         return (
             <Paper className="blogPostPaper">
                 <div className="blogPost">
                     <Gravatar md5={this.props.post.gravatar} /><h2 className="title"><a onClick={this.props.onClick}>{this.props.post.title}</a></h2>
-                    <span>Submitted by {this.props.post.createUserId} on {this.props.post.createDate}</span>
+                    <span>Submitted by {this.props.post.createUserId} on {time}</span>
                     <Markdown text={this.props.post.summary} />
                 </div>
             </Paper>
