0fc281968cfd676359377a549ee65cb9b7029340
Steve Hu <stevehu@gmail.com>
2016-02-13 06:02:52 -0500

    layout for the product summary

--- demo/src/app/components/catalog/Catalog.js
+++ demo/src/app/components/catalog/Catalog.js
@@ -123,14 +123,6 @@ var Catalog = React.createClass({
                         }
                         <Pagination locale={Locale} selectComponentClass={Select} showSizeChanger={true} pageSizeOptions={['10', '25', '50', '100']} onShowSizeChange={this._onPageSizeChange} onChange={this._onPageNoChange} current={this.state.pageNo} pageSize={this.state.pageSize} total={this.state.total}/>
                     </div>
-                    <div className="rightColumn">
-                        <div className="blogInfo">
-                            <h1>Blog Information</h1>
-                            <p>In this section, you will see some information and references pertaining to the opened blog.</p>
-                            <p>Also, having the screen width be less then 64em will hide it, leaving reading room for mobile users only concerned with reading post content on the go.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci alias cum, cumque cupiditate ea eum itaque, minus molestias necessitatibus nihil pariatur perspiciatis quam quas quod rem repellat, sint voluptate.</p>
-                        </div>
-                    </div>
                 </div>
             </div>
         );
