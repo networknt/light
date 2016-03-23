1d4044a9f7ba8306835b6eec2b49750ed8d8a3df
Steve Hu <stevehu@gmail.com>
2016-03-23 06:54:45 -0400

    update wip

--- networknt/src/app/components/comment/CommentForm.jsx
+++ networknt/src/app/components/comment/CommentForm.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TextField from 'material-ui/lib/TextField';
 
 var CommentForm = React.createClass({
+
 
     propTypes: {
         parentRid: React.PropTypes.string
@@ -22,7 +24,7 @@ var CommentForm = React.createClass({
     render: function() {
         return (
             <form className='new-comment-form' onSubmit={this.handleSubmit}>
-                <textarea type='text' ref='message' maxLength='300' placeholder='Reply to thread...' className="thread-reply-textarea"/>
+                <TextField hintText="Reply to thread..." multiLine={true} rows={4} ref='message'/>
                 <br />
                 <input type="submit" value="reply" />
             </form>
