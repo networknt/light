71d35458fe598098d225ad181fa5ea1ea940742c
Steve Hu <stevehu@gmail.com>
2016-03-23 09:23:17 -0400

    reply form tabed and width 100%

--- networknt/src/app/components/comment/CommentForm.jsx
+++ networknt/src/app/components/comment/CommentForm.jsx
@@ -1,35 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TextField from 'material-ui/lib/TextField';
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Markdown from '../Markdown';
 
 var CommentForm = React.createClass({
-
 
     propTypes: {
         parentRid: React.PropTypes.string
     },
 
-    handleSubmit: function(e) {
+    getInitialState: function() {
+        return {
+            value: ''
+        };
+    },
+
+    onReply: function(e) {
         e.preventDefault();
-
-        var message = ReactDOM.findDOMNode(this.refs.message).value.trim();
-
-        if (!message) {
+        if (!this.state.value || this.state.value.length === 0) {
             return;
         }
-
-        this.props.onCommentSubmit(this.props.parentRid, message);
-        ReactDOM.findDOMNode(this.refs.message).value = '';
+        this.props.onCommentSubmit(this.props.parentRid, this.state.value);
+        this.setState({value: ''});
     },
+
+    onChange: function(e) {
+        this.setState({
+            value: e.target.value
+        })
+    },
+
     render: function() {
         return (
-            <form className='new-comment-form' onSubmit={this.handleSubmit}>
-                <TextField hintText="Reply to thread..." multiLine={true} rows={4} ref='message'/>
-                <br />
-                <input type="submit" value="reply" />
-            </form>
+            <div style={{width: '100%'}}>
+                <Tabs initialSelectedIndex={0}>
+                    <Tab label="Content">
+                        <TextField style={{width: '100%'}} hintText="Reply to thread..." multiLine={true} rows={4} onChange={this.onChange} />
+                    </Tab>
+                    <Tab label="Review">
+                        <Markdown text={this.state.value} />
+                    </Tab>
+                </Tabs>
+                <RaisedButton primary={true} label="Reply" onTouchTap={this.onReply} />
+            </div>
         );
     }
 });
 
 module.exports = CommentForm;
+
+/*
+ <form style={{width: '100%'}} className='new-comment-form' onSubmit={this.handleSubmit}>
+ <Tabs initialSelectedIndex={1}>
+ <Tab label="Content">
+ <TextField hintText="Reply to thread..." multiLine={true} rows={4} onChange={this.onChange} />
+ </Tab>
+ <Tab label="Review">
+ <Markdown text={this.state.value} />
+ </Tab>
+ </Tabs>
+
+ <br />
+ <input type="submit" value="reply" />
+ </form>
+
+ */
